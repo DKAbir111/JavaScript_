a8d6d7e1d77c9b03cb7a1e9a2558e8175652df48
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-08-31 21:43:05 +0600

    Array some functionality

---
+++ array_index.js
@@ -0,0 +1,10 @@
+const numbers=[88,65,34,12,98,9];
+console.log(numbers);
+console.log(numbers[1]);
+const second=numbers[1];
+console.log(second);
+
+
+//Set or update element
+numbers[1]=78;
+console.log(numbers);
---
+++ array_length.js
@@ -0,0 +1,2 @@
+const number=[1,4,5,6,8,5,4,2]
+console.log(number.length)
